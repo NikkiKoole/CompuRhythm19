958db4a214a2a386042c59e2f6ed5fc10f76270d
Nikki Koole <nikkikoole@gmail.com>
2019-03-20 01:16:14 +0100

    wonky stuff

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 inspect = require "inspect"
 require 'simple-slider'
 
-colorDivider = 1.0
+colorDivider = 255
 
 function love.keypressed(key)
    if key == "escape" then
@@ -42,8 +42,8 @@ function love.load()
       {name='Kick', sound=love.audio.newSource('samples/Kick.wav', 'static')},
       {name='Kick accent', sound=love.audio.newSource('samples/Kick Accent.wav', 'static')},
    }
-
-   addBars(pattern, 32)
+   totalLength = 32
+   addBars(pattern, totalLength)
    bpm = 300
    swing = 50 -- percentage of swing, 50 == 0 (robert linn's way of doing swing)
    pitch = 1.0
@@ -56,7 +56,11 @@ function love.load()
    drawingValue = 1
    cellWidth = 24
    cellHeight = 32
+   openedInstrument = 0
 
+
+   customPitch = 0
+   customSwing = 50
    --success, message = love.filesystem.write("wrote_this.txt", inspect(pattern, {indent=""}))
    --path = love.filesystem.getAppdataDirectory( )
    --print(path)
@@ -72,7 +76,7 @@ function love.load()
 			 {track="line"})
 
    pitchSlider = newSlider(100+gridMarginLeft + 320, 710 ,
-			 200, pitch, 0, 10.0,
+			 200, pitch, 0, 1.0,
 			 function(v) pitch=v end,
 			 {track="line"})
 
@@ -81,6 +85,33 @@ function love.load()
 			 function(v) pitchRandom=v end,
 			 {track="line"})
 
+   customPitchSlider = newSlider(gridMarginLeft, 50  ,
+			 100, customPitch, 0, 3.0,
+			 function(v)
+			    if openedInstrument > 0 then
+			       pattern[openedInstrument].pitch = v
+			       --print("wanna do stuff!", v)
+			       --customPitch=pattern[openedInstrument].pitch
+			    end
+			    customPitch = v
+			    
+			 end,
+			 {track="line"})
+
+    customSwingSlider = newSlider(200 + gridMarginLeft, 50  ,
+			 100, customSwing, 50, 100,
+			 function(v)
+			    if openedInstrument > 0 then
+			       pattern[openedInstrument].swing = v
+			       --print("wanna do stuff!", v)
+			       --customPitch=pattern[openedInstrument].pitch
+			    end
+			    customSwing = v
+			    
+			 end,
+			 {track="line"})
+    
+   --print(inspect(customPitchSlider))
 end
 
 
@@ -89,13 +120,17 @@ function addBars(pattern, count)
    pattern.length = count
    for i = 1, #pattern do
       pattern[i].values = {}
+      pattern[i].volume = 1.0
+      pattern[i].muted = false
+      pattern[i].pitch = 1.0
+       pattern[i].swing = 50
       for j = 0, count do
 	 table.insert(pattern[i].values, false)
       end
    end
 end
 
-function handlePress(x,y, value)
+function handlePressInGrid(x,y, value)
    local row, index = getRowAndIndex(x,y)
    if row > -1 and index > -1 then
       if value ~= nil then
@@ -126,16 +161,59 @@ function love.mousepressed(x, y)
    -- do that for all the cells touched by the subsequent move
    local row, index = getRowAndIndex(x,y)
 
+   if openedInstrument == 0 then
    if row > -1 and index > -1 then
       local value = pattern[row].values[index]
       drawingValue = not value
-      handlePress(x,y, drawingValue)
+      handlePressInGrid(x,y, drawingValue)
    end
+   end
+
+   if (x > 20 and x < gridMarginLeft) and
+      (y > gridMarginTop and y < gridMarginTop  + cellHeight* #pattern) then
+	 local lx = x
+	 local ly = y - gridMarginTop
+	 local index = math.floor(ly / cellHeight) + 1
+
+	 if openedInstrument == index then
+	    openedInstrument = 0
+	 else
+	    openedInstrument = index
+	 end
+
+	 customPitchSlider = newSlider(gridMarginLeft + 130,
+				       index * cellHeight + gridMarginTop - 20  ,
+			 100, (pattern[index]).pitch, 0, 3.0,
+			 function(v)
+			    if openedInstrument > 0 then
+			       pattern[openedInstrument].pitch = v
+			    end
+			    customPitch = v
+			    
+			 end,
+			 {track="line"})
+
+	 customSwingSlider = newSlider(gridMarginLeft + 130 + 130,
+				       index * cellHeight + gridMarginTop - 20  ,
+			 100, (pattern[index]).swing, 50, 100,
+			 function(v)
+			    if openedInstrument > 0 then
+			       pattern[openedInstrument].swing = v
+			    end
+			    customSwing = v
+			    
+			 end,
+			 {track="line"})
+   end
+   
+
+   -- check if you click an instrument to open its panel
+   --love.graphics.rectangle('line',20,  gridMarginTop + (i-1)*cellHeight, gridMarginLeft-20, cellHeight )
 end
 function love.mousemoved(x,y)
    local down = love.mouse.isDown( 1)
-   if down then
-      handlePress(x,y, drawingValue)
+   if down and openedInstrument == 0 then 
+      handlePressInGrid(x,y, drawingValue)
    end
 end
 
@@ -154,6 +232,7 @@ function love.update(dt)
 	    timeToAdd = timeToAdd * -1
 	 end
       end
+      
 
       if (timeInBeat >= (60/bpm + timeToAdd)) then
 	 playhead = playhead + 1
@@ -189,14 +268,23 @@ function love.update(dt)
 		  local r = love.math.random() * 12
 		  return math.floor(r) * (1.0/12)
 	       end
-	       sfx:setPitch(math.max(p, 0.00001))
+
+	       local layerPitch = pattern[i].pitch
+	       print(layerPitch)
+	       sfx:setPitch(math.max(p*layerPitch, 0.00001) )
 
 	       --sfx:setPitch(math.max(getRandomNotePitch(), 0.000001))
 
 	       --end
-		  --sfx:setPitch(love.math.random())
-
-		  sfx:play()
+	       --sfx:setPitch(love.math.random())
+	       
+	       --love.audio.setPosition( 1 - love.math.random() * 2, 0, 0 )
+	       love.audio.setVolume(1)
+	       if playhead % 4 == 0 then
+		  love.audio.setVolume(1.2)
+	       end
+	       
+	       sfx:play()
 	       end
 	 end
 
@@ -209,6 +297,9 @@ function love.update(dt)
    swingSlider:update()
    pitchSlider:update()
    pitchRandomSlider:update()
+   customPitchSlider:update()
+   customSwingSlider:update()
+   
 end
 
 
@@ -218,12 +309,12 @@ function love.draw()
    love.graphics.setColor(35/colorDivider,36/colorDivider,38/colorDivider)
    love.graphics.setLineWidth( 2)
 
-
+   
    for i =1, #pattern do
-
-      love.graphics.print(pattern[i].name,
-			  20, gridMarginTop + (i-1) * cellHeight)
-
+     
+      
+      love.graphics.print(pattern[i].name,  20, gridMarginTop + (i-1) * cellHeight)
+      love.graphics.rectangle('line',20,  gridMarginTop + (i-1)*cellHeight, gridMarginLeft-20, cellHeight )
       --we assume some width is enough to fit all the names say 100
       for j=1, #(pattern[i].values)-1 do
 	 if (pattern[i].values[j]) then
@@ -240,6 +331,21 @@ function love.draw()
       end
 
    end
+
+   if openedInstrument > 0 then
+      --print(openedInstrument)
+      love.graphics.setColor(255/colorDivider,0/colorDivider,0/colorDivider)
+      love.graphics.rectangle('fill',
+				    gridMarginLeft,
+				    gridMarginTop + (openedInstrument-1)*cellHeight,
+				    cellWidth * totalLength , cellHeight )
+      love.graphics.setColor(0/colorDivider,0/colorDivider,0/colorDivider)
+      love.graphics.print('pitch: '.. pattern[openedInstrument].pitch, gridMarginLeft,  gridMarginTop + (openedInstrument-1)*cellHeight)
+      customPitchSlider:draw()
+
+      customSwingSlider:draw()
+   end
+   
 
    if playing then
       love.graphics.setColor(255/colorDivider,255/colorDivider,255/colorDivider)
